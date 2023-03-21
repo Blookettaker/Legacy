0a310796ba46ee6f27db989a240db4f8bcf6f5aa
Blookettaker <125590478+Blookettaker@users.noreply.github.com>
2023-03-21 15:25:54 -0500

    Update spoofAdmin.js

--- utils/spoofAdmin.js
+++ utils/spoofAdmin.js
@@ -1,22 +1,21 @@
 if (location.pathname === '/stats/') {
-  document.getElementById("role").innerText = "Role: Owner";
-  document.getElementById("tokens").innerText = "Tokens: Infinite";
-  document.getElementById("userBlook").src = "https://blacket.org/images/blacketImageRainbow.gif";
+  document.getElementById("role").innerText = "Role: Rich";
+  document.getElementById("tokens").innerText = "Tokens: 7381468394419652671015037282551615518";
+  document.getElementById("userBlook").src = "https://legacy.blacket.org/blooks/Space/Tim%20The%20Alien.gif";
 } else if (location.pathname === '/leaderboard/') {
   $.get(`/worker/user/getusername.php`, function(data) {
     if (document.getElementById("preloadLeaderboard").innerHTML.split('<br>2. ')[1].includes(data)) {
         let x = document.getElementById("preloadLeaderboard").innerHTML.split('<br>2. ')[1].replace(data, "asdfghjkl");
-        document.getElementById("preloadLeaderboard").innerHTML = `1. ${data} - Infinite Tokens<br>2. ` + x;
+        document.getElementById("preloadLeaderboard").innerHTML = `1. ${data} - 7381468394419652671015037282551615518Tokens<br>2. ` + x;
     } else {
         let x = document.getElementById("preloadLeaderboard").innerHTML.split('<br>2. ')[1];
-        document.getElementById("preloadLeaderboard").innerHTML = `1. ${data} - Infinite Tokens<br>2. ` + x;
+        document.getElementById("preloadLeaderboard").innerHTML = `1. ${data} - 7381468394419652671015037282551615518Tokens<br>2. ` + x;
     };
   });
 } else if (location.pathname === '/market/') {
-  document.getElementById("tokensText").innerHTML = 'INFINITE';
+  document.getElementById("tokensText").innerHTML = '7381468394419652671015037282551615518';
 } else if (location.pathname === '/blooks') {
   setInterval(() => {
     document.getElementById("blookQuantity").innerHTML = 'Quantity: Infinite';
   }, 10);
-};
-$(`<a id="adminSpoofBtn" class="styles__navContainer___1sGPx-camelCase" onclick='alert("This is not an admin giver, just a spoof.")'><i class="styles__statsIcon___j0Lcd-camelCase fas fa-wrench"></i></a>`).appendTo('.styles__headerRight___1Qwu1-camelCase');
+};class="
